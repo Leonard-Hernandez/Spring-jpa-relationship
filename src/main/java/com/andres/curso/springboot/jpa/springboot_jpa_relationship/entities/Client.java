d8429642dd42ede1e1f5ab0f7c17b76a1a72450b
Leonard <Leonardhernandez02@gmail.com>
2025-03-02 17:19:10 -0500

    creando entidades

---
+++ src/main/java/com/andres/curso/springboot/jpa/springboot_jpa_relationship/entities/Client.java
@@ -0,0 +1,52 @@
+package com.andres.curso.springboot.jpa.springboot_jpa_relationship.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String lastName;
+
+    public Client() {}
+
+    public Client(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
